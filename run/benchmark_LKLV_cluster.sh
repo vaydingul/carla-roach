f5d9ee3fc210833300e58744eeda84c89e9b182b
Volkan Aydıngül <volkanaydingul@gmail.com>
2022-07-24 16:15:01 +0300

    Benchmark with different seeds

--- run/benchmark_LKLV_cluster.sh
+++ run/benchmark_LKLV_cluster.sh
@@ -55,10 +55,10 @@
    wb_project=iccv21-roach-benchmark \
    agent=$agent actors.hero.agent=$agent \
    agent.cilrs.wb_run_path=vaydingul/il_leaderboard_roach/2rjhvifn \
-   'wb_group="L_K + L_V scratch (2)"' \
+   'wb_group="L_K + L_V scratch (3)"' \
    'wb_notes="Benchmark L_K + L_V on Leaderboard New Weather/ New Town."' \
    test_suites=lb_test_nn_new \
-   seed=2021 \
+   seed=2022 \
    +wb_sub_group=leaderboard_dense-2021 \
    no_rendering=false \
    carla_sh_path=${CARLA_ROOT}/CarlaUE4.sh
