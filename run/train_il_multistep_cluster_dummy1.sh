3c3ee22dda0c0d4605eac5376e2870d43503bfb0
Volkan Aydıngül <volkanaydingul@gmail.com>
2022-07-27 02:01:34 +0300

    Batch size correction

--- run/train_il_multistep_cluster_dummy1.sh
+++ run/train_il_multistep_cluster_dummy1.sh
@@ -37,7 +37,7 @@ agent.cilrs.env_wrapper.kwargs.value_as_supervision=true \
 agent.cilrs.training.kwargs.value_weight=0.001 \
 agent.cilrs.env_wrapper.kwargs.dim_features_supervision=256 \
 agent.cilrs.training.kwargs.features_weight=0.0 \
-agent.cilrs.training.kwargs.batch_size=192 \
+agent.cilrs.training.kwargs.batch_size=64 \
 agent.cilrs.training.kwargs.num_workers=2 \
 cache_dir="$1"
 }
