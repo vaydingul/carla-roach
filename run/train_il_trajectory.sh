f66b1f4e90b742a530671c4fb63284d857a5184f
Volkan Aydıngül <volkanaydingul@gmail.com>
2022-08-09 07:11:44 +0300

    Run script

--- run/train_il_trajectory.sh
+++ run/train_il_trajectory.sh
@@ -41,7 +41,7 @@ agent.cilrs.training.kwargs.batch_size=1 \
 agent.cilrs.training.kwargs.num_workers=4 \
 agent.cilrs.policy.kwargs.use_multi_step_control=true \
 agent.cilrs.policy.kwargs.use_multi_step_waypoint=true \
-agent.cilrs.policy.kwargs.initial_hidden_zeros=false \
+agent.cilrs.policy.kwargs.initial_hidden_zeros=true \
 cache_dir="$1"
 }
 
