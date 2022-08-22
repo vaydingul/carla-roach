90f7d94a7159c3cda0a23cfa014836c268ea5173
Volkan Aydingul <vaydingul20@ku.edu.tr>
2022-08-22 04:00:29 +0300

    Temporal module case

--- run/train_il_trajectory_target_cluster_3.sh
+++ run/train_il_trajectory_target_cluster_3.sh
@@ -25,7 +25,7 @@
 train_il () {
 
 python -u train_il.py reset_step=true \
-wb_project="il_leaderboard_roach" wb_group="train_trajectory_with_target_waypoint" 'wb_name="Trajectory Branch - Temporal Module"' \
+wb_project="il_leaderboard_roach" wb_group="train_trajectory_with_target_waypoint_new_data" 'wb_name="Trajectory Branch - Temporal Module"' \
 dagger_datasets=["/scratch/users/vaydingul20/carla-dataset-detailed/"] \
 agent.cilrs.env_wrapper.kwargs.input_states=[speed,vec,cmd] \
 agent.cilrs.policy.kwargs.number_of_branches=1 \
